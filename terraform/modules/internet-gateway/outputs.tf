febbf1dbb3621ef2e0f9cfd87fd9ff4742f9a11d
peerezjoao <peerezjoao1@gmail.com>
2024-08-30 21:30:33 -0300

    fix: add new output from internet_gateway

--- terraform/modules/internet-gateway/outputs.tf
+++ terraform/modules/internet-gateway/outputs.tf
@@ -1,4 +1,9 @@
 output "internet_gateway_name" {
-    value = aws_internet_gateway.gateway.id
+    value = aws_internet_gateway.internet_gateway
     description = "Output gateway internet name"
+}
+
+output "internet_gateway_id" {
+    value = aws_internet_gateway.internet_gateway.id
+    description = "Output gateway intenernet gateway id."
 }
